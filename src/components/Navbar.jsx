21e3aeffd25775697b0a7ef511f7f958d3160eee
unknown <najmulcse247@gmail.com>
2025-01-19 23:15:50 +0600

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,22 +2,28 @@ import React from "react";
 import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
-    <nav>
-      <ul className="flex py-6 px-4 gap-6">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/shop">Shop</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="flex mx-auto justify-between py-4 px-6 container max-w-screen-2xl items-center">
+        <Link to="/">Logo</Link>
+        <div>
+          <ul className="flex gap-6">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/shop">Shop</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <div>cart</div>
+      </nav>
+    </header>
   );
 };
 
