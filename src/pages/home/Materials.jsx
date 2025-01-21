537a052dd4e74ec5c0e28c213c388ea41daeaf31
unknown <najmulcse247@gmail.com>
2025-01-21 21:20:28 +0600

    experience done

--- src/pages/home/Materials.jsx
+++ src/pages/home/Materials.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import experienceImg from "../../assets/material1.png"
+import experienceImg1 from "../../assets/material1.png"
+import experienceImg2 from "../../assets/material2.png"
+import experienceImg3 from "../../assets/material3.png"
 
 const Materials = () => {
     return (
@@ -11,13 +13,13 @@ const Materials = () => {
                 <p className='text-second dark:text-white mb-2 lg:w-2/3'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quae reprehenderit architecto aut ipsa tenetur, voluptates iusto consequuntur non. Ducimus, officia illo. Fuga beatae quod hic inventore eius quis aspernatur vel?</p>
                 <button className='border border-l-rose-50 px-4 py-2 rounded hover:bg-primary hover:text-white transition ease-linear'>More Info</button>
             </div>
-            <div className='md:w-1/2 md:h-[541px]'>
+            <div className='md:w-1/2 md:h-[541px] grid grid-cols-2 items-end'>
                 <div>
-                    <img src={experienceImg} alt="" className='w-full h-full' />
-                    <img src={experienceImg} alt="" className='w-full h-full' />
+                    <img src={experienceImg1} alt="" />
+                    <img src={experienceImg2} alt="" />
                 </div>
                 <div>
-                    <img src={experienceImg} alt="" className='w-full h-full' />
+                    <img src={experienceImg3} alt="" className='w-full md:h-[541px]' />
                 </div>
             </div>
         </section>
